81d94bab8a60d3487ebdf597c29cba2e87e197a3
Myungjin Song <reki318@naver.com>
2018-01-12 00:49:54 +0900

    header nav direction build

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,35 +5,52 @@ import Introduce from "../ContentPage/Introduce";
 import Business from "../ContentPage/Business";
 import Product from "../ContentPage/Product";
 import Customer from "../ContentPage/Customer";
+import styles from "./header.scss";
+import logo from "../../Image/metrologo.png";
 
 
 class Header extends Component {
     render() {
-        return (
-        <header>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/introduce">회사소개</Link>
-                </li>
-                <li>
-                  <Link to="/business">사업영역</Link>
-                </li>
-                <li>
-                  <Link to="/product">제품소개</Link>
-                </li>
-                <li>
-                  <Link to="/customer">고객센터</Link>
-                </li>
-              </ul>
-            </nav>
+        return <div>
+            <div className={styles.header}>
+            <div className={styles.container}>
+              <div className={styles.branding}>
+                <Link to = "/">
+                <img className={styles.boxImg} src={logo} alt="logo" />
+                </Link>
+              </div>
+              <nav>
+                <ul className={styles.ul}>
+                  <li className={styles.highlight}>
+                    <Link className={styles.context} to="/introduce">
+                      회사소개
+                    </Link>
+                  </li>
+                  <li className={styles.highlight}>
+                    <Link className={styles.context} to="/business">
+                      사업영역
+                    </Link>
+                  </li>
+                  <li className={styles.highlight}>
+                    <Link className={styles.context} to="/product">
+                      제품소개
+                    </Link>
+                  </li>
+                  <li className={styles.highlight}>
+                    <Link className={styles.context} to="/customer">
+                      고객센터
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              </div>
+            </div>
             <Route exact path="/" component={Home} />
             <Route path="/introduce" component={Introduce} />
             <Route path="/business" component={Business} />
             <Route path="/product" component={Product} />
             <Route path="/customer" component={Customer} />
-        </header>
-        );
+          </div>;
     }
 }
 
