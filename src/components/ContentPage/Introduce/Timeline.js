52037599c5eba5cfcd3282ca97bcd22bb1681819
Myungjin Song <reki318@naver.com>
2018-01-23 18:06:05 +0900

    introduce of company

--- src/components/ContentPage/Introduce/Timeline.js
+++ src/components/ContentPage/Introduce/Timeline.js
@@ -1,8 +1,6 @@
 import React from "react";
-import propTypes from "prop-types";
 import styles from "./styles.scss";
 import { Timeline, TimelineEvent } from "react-event-timeline";
-import * as actions from "../../../actions/ActionTypes";
 
 
 class Timeliner extends React.Component {
@@ -15,7 +13,63 @@ class Timeliner extends React.Component {
   
     this._loadContent = this._loadContent.bind(this);
     this._loadDetail = this._loadDetail.bind(this);
+    this._loadTitle = this._loadTitle.bind(this);
   };
+
+  _loadTitle = obj => {
+    
+    const style = {
+     // padding: "inherit"
+      padding: "10px",
+      marginTop : "10px",
+      marginRight : "20px"
+    }
+
+    const box = {
+      fontWeight : "bold",
+      padding: "2px",
+      left: "-2.5em",
+      top: "50%",
+      margiTop: "-1em",
+      background: "#169b9b",
+      width: "6em",
+      height: "2em",
+      lineHeight: "2em",
+      textAlign: "center",
+      borderRadius : "5%"
+    }
+
+
+    return <div className={styles.boxes}>
+        <div className={styles.container}>
+          <div>
+            <ol className={styles.rectangleList}>
+              <li>
+                <div style={box}>사업자명</div>{" "}<a href="">{obj["company"]}</a>
+              </li>
+              <li>
+                <div style={box}>대표자</div><a href="">{obj["ceo"]}</a>
+              </li>
+              <li>
+                <div style={box}>사업자분야</div><a href="">{obj["area"]}</a>
+              </li>
+              <li>
+                <div style={box}>주소</div><a href="">{obj["address"]}</a>
+              </li>
+              <li>
+                <div style={box}>전화번호</div><a href="">{obj["tel"]}</a>
+              </li>
+              <li>
+                <div style={box}>회사설립연도</div><a href="">{obj["birth"]}</a>
+              </li>
+              <li>
+                <div style={box}>홈페이지</div><a href="">{obj["homepage"]}</a>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>;
+  }
 
   _loadDetail = obj => {
     var total = "";
@@ -43,7 +97,7 @@ class Timeliner extends React.Component {
 
     const title = { fontSize: "30px", fontWeight: "bold", color: "#363636" };
     const bubbleStyle = { position: "absolute", top: "0px", left: "0px", borderRadius: "50%", width: "30px", height: "30px", marginLeft: "1px", background: "#fff", border: "3px solid #169b9b", display: "flex" };
-    const contentStyle = { fontSize: "15px", fontWeight: "400", width: "auto" };
+    const contentStyle = { fontSize: "15px", fontWeight: "400", width: "auto" ,backgroundColor : "none", boxShadow : "none" };
 
     return (
     content.reverse().map((object , i ) => {
@@ -61,6 +115,7 @@ class Timeliner extends React.Component {
     
     return (
       <div className={styles.TimelineStyle}>
+          {this._loadTitle(this.state.object["init"])}
         <Timeline>
           {this._loadContent(this.state.object)}
         </Timeline>
