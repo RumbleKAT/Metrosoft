9d5aa9a34dd56d8a0a36eb5e6662a8b3caee591c
Myungjin Song <reki318@naver.com>
2018-01-31 17:07:09 +0900

    address table build

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -154,7 +154,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.(css|scss)$/,
+            test: /\.(scss|sass|css)$/,
             use: [
               require.resolve("style-loader"),
               {
@@ -184,8 +184,7 @@ module.exports = {
                     })
                   ]
                 }
-              },
-              {
+              },{
                 loader: require.resolve("sass-loader"),
                 options: {
                   includePaths: [paths.styles]
--- src/components/ContentPage/Customer/index.js
+++ src/components/ContentPage/Customer/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import CustomerTable from "./CustomerTable";
+import PointDiv from "../pointDiv";
+
+const address = [
+  {
+    title: "부서",
+    content: ["대표", "대표전화/고객문의", "", "customer@metrosoft.co.kr/031-465-9971~3"]
+  },
+  {
+    title: "성명",
+    content: ["병원 영업", "이용구", "이사", "yglee@metrosoft.co.kr/010-8877-2676"]
+  },
+  {
+    title: "직위",
+    content: ["기술 연구소", "노우창", "부장", "wcrho@metrosoft.co.kr"]
+  },
+  {
+    title: "이메일 주소",
+    content: ["개발" , "이우일" , "부장","wooil@metrosoft.co.kr"]
+  },
+  {
+    content: ["통신(VOIP)","서진원","팀장","jinwon@metrosoft.co.kr"]
+  }
+
+];
 
 class Customer extends Component {
     render() {
         return (
-            <div>
-                <h1>사업영역</h1>
+            <div style={{marginTop:"30px"}}>
+                <PointDiv onTitle={"고객지원"}/>
+                <CustomerTable onContent={address}/>
             </div>
         );
     }
