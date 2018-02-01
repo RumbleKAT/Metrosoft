a6619740b6232559e9ceb37ebb6ed5fd1f0cd404
Myungjin Song <reki318@naver.com>
2018-02-02 01:44:53 +0900

    MetroHIS List added

--- src/components/ContentPage/Business/MetroHIS.js
+++ src/components/ContentPage/Business/MetroHIS.js
@@ -12,6 +12,24 @@ const titleStyle = (obj) => {
     );
 };
 
+const features = [
+    "병원시스템의 전 부문을 포함하도록 설계되었습니다",
+    "환자의 진료 정보를 중심으로 설계되었습니다",
+    "의사의 처방 자료 및 진료기록(차트)가 통합되었습니다",
+    "의사, 간호사, 기사 등 모든 사용자 직군에서 다년간 현장에서 검증이 완료되었습니다",
+    "각종 의료장비와의 연동을 보장함으로써 사용자 편의성을 최대화합니다",
+    "전자의무기록 서비스의 도립으로 인한 병원 전반의 Paper Work가 감소합니다."
+];
+
+const effects = [
+    "획기적인 환자 서비스 개선",
+    "병원 수익 증대 및 비용 절감",
+    "합리적 경쟁평가를 위한 평가지표 제공",
+    "업무 생산성 향상",
+    "정보의 정확도, 신속성, 인력 운영의 효율성 증대",
+    "의학연구정보의 체계적 관리"
+]
+
 const selectedcolors = ["#64b764", "#649dc2", "#feb40e", "#9fc543"];
 
 const contentStyle = {
@@ -22,15 +40,17 @@ const contentStyle = {
     height:"210px"
 }
 
- 
 class MetroHIS extends Component {
 
     constructor(props){
         super(props);
         this.loadComposition = this.loadComposition.bind(this);
+        this.loadTitle = this.loadTitle.bind(this);
     }
+
+
     loadComposition(){
-            return <div style={{ padding: "0px", overflow: "hidden" }}>
+            return <div style={{ padding: "0px", overflow: "hidden" , marginTop:"50px" }}>
                 <div className="ui green segment" style={contentStyle}>
                   <h2 style={titleStyle(selectedcolors[0])}>
                     원무
@@ -95,6 +115,29 @@ class MetroHIS extends Component {
               </div>;
     }
 
+    loadTitle(obj){
+        return <div className="ui label">
+            <i className="chevron right icon" />{obj}
+          </div>;
+    }
+
+    loadList(objects){
+
+        return(
+            <div style={{padding:"10px" , marginTop: "20px"}}>
+                <ul className="ui list">
+                {
+                    objects.map((obj,i) => {
+                        return <div>
+                            <li key={i}>{obj}</li><br/>
+                          </div>;
+                    })
+                }
+                </ul>
+            </div>
+            );
+    }
+
     render() {
         return <div style={{ padding: "50px" }}>
             <PointDiv onTitle={"의료정보사업"} />
@@ -105,18 +148,22 @@ class MetroHIS extends Component {
                 MetroHIS는 OCS, EMR, HealthCare, CRM, EIS, mPocs 등의 다양한 제품 군을 가지고 병원실정에 맞는 패키지 의료정보 기능을 제공합니다.
               </span>
               <div style={{ marginTop: "50px" }}>
-                <div className="ui label">
-                  <i className="chevron right icon" />사업내용
-                </div>
+                {this.loadTitle("사업내용")}
                 <div style={{ padding: "10px", margin: "10px auto 0px auto", width: "40%", minWidth: "320px" }}>
                   <img style={{ marginRight: "50px" }} src={BusinessContent} alt="사업 내용 그림" />
                 </div>
               </div>
               <div style={{ marginTop: "50px" }}>
-                <div className="ui label">
-                  <i className="chevron right icon" />의료정보사업의 구성
-                </div>
+                {this.loadTitle("의료정보사업의 구성")}
                 {this.loadComposition()}
+              </div>
+              <div style={{ marginTop: "50px" }}>
+                {this.loadTitle("특징")}
+                {this.loadList(features)}
+              </div>
+              <div style={{ marginTop: "50px" }}>
+                {this.loadTitle("도입효과")}
+                {this.loadList(effects)}
               </div>
             </div>
           </div>;
